95903fb4bbbbfcfafa2885a6afca523062eeeba3
dasharath bhukya <dasharath.b@gmail.com>
2017-02-09 09:17:34 +0530

    added new guest page

---
+++ src/pages/newguest/newG.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'new-guest',
+  templateUrl: 'newG.html'
+})
+export class NewGuest {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+}
