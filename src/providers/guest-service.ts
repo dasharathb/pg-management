5d32e6697d25249daf6418cca6691a571fc2b81c
dasharath bhukya <dasharath.b@gmail.com>
2017-03-10 07:51:41 +0530

    added post webservice

--- src/providers/guest-service.ts
+++ src/providers/guest-service.ts
@@ -1,4 +1,4 @@
-import { Http } from '@angular/http';
+import { Http, Response, Headers, RequestOptions } from '@angular/http';
 
 export class GuestService {
 
@@ -10,10 +10,12 @@ export class GuestService {
   }
   public registerGuest(person) {
     console.log('person:::',person);
-        var url = 'http://192.168.1.5:9080/pg-management/register/guest';
+        var url = 'http://localhost:9080/pg-management/register/guest';
         //var url = 'http://192.168.1.5:9020/my-events-server/search';
         //var url = 'http://localhost:9020/my-events-server/search';
-        var response = this.http.post(url, person).map(res => res.json());
+        let headers      = new Headers({ 'Content-Type': 'application/json' }); // ... Set content type to JSON
+        let options       = new RequestOptions({ headers: headers });
+        var response = this.http.post(url, JSON.stringify(person), options).map(res => res.json());
         return response;
     }
 
