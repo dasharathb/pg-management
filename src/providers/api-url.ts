b149be3166270d5d72f8c22e537df5d19f05fe2a
dasharath bhukya <dasharath.b@gmail.com>
2017-10-31 09:00:24 +0530

    changes to support cordova ionic new version

--- src/providers/api-url.ts
+++ src/providers/api-url.ts
@@ -1,4 +1,4 @@
 
 //export const API_ENDPOINT = 'http://localhost:9080/pg-management';
 //export const API_ENDPOINT = 'http://183.83.128.106:9080/pg-management';
-export const API_ENDPOINT = 'http://ec2-54-68-129-6.us-west-2.compute.amazonaws.com:9080/pg-management';
+export const API_ENDPOINT = 'http://ec2-34-208-14-122.us-west-2.compute.amazonaws.com:9080/pg-management';
